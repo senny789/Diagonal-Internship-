d7b0cda2690add9d6d7e816296500d5713d7c2a0
sushan sen <sen.sushan@gmail.com>
2022-04-30 09:14:32 +0545

    added comments

--- Task2.js
+++ Task2.js
@@ -1,4 +1,4 @@
-
+//Code used for reference:
 // const formulatePayment = (choice, amount) => {
 // const initialPayment = ((10 / 100) * amount).toFixed(2); const remainingTotal = amount - initialPayment;
 // if (choice == 'weekly') {
@@ -21,7 +21,7 @@
 //         monthly = Math.ceil(months);
 //       }
 // }
-// console.log(`You will be paid ${monthlyPrice} for $ {monthly} months.`);
+// console.log(`You will be paid ${monthlyPrice} for ${monthly} months.`);
 // return { time: monthly, amount: monthlyPrice }; } else {
 // return null; }
 // };
@@ -31,11 +31,11 @@ const formulatePayment = (choice, amount) => {
     const initialPayment = ((10 / 100) * amount).toFixed(2); 
     const remainingTotal = amount - initialPayment;
     if (choice == 'weekly') {
-          let weeklyAmountPayment = 50;
+          let weeklyAmountPayment = 50;         /* changing the value from 0 to 50 since a multiple of 50 is needed for the payment during weeks */
           let weekly = remainingTotal / weeklyAmountPayment;
           while (weekly > 156) {
             weeklyAmountPayment = weeklyAmountPayment + 50; 
-            weekly = remainingTotal / weeklyAmountPayment; 
+            weekly = remainingTotal / weeklyAmountPayment; /* here instead of declaring a new variable we can use the existing weekly variable */ 
             if (weekly % 50 !== 0) {
                   weekly = Math.ceil(weekly);
                 }
@@ -44,11 +44,11 @@ const formulatePayment = (choice, amount) => {
           return { time: weekly, amount: weeklyAmountPayment }; 
         } 
     else if (choice == 'monthly') {
-        let monthlyPrice = 100;
+        let monthlyPrice = 100; /* changing the value from 0 to 100 since a multiple of 100 is needed for the payment during months */
         let monthly = remainingTotal / monthlyPrice;
          while (monthly > 36) {
              monthlyPrice = monthlyPrice + 100;
-                monthly = remainingTotal / monthlyPrice;
+                monthly = remainingTotal / monthlyPrice; /* here instead of declaring a new variable we can use the existing monthly variable */ 
              if (monthly % 100 !== 0) {
                     monthly = Math.ceil(monthly);
                  }
